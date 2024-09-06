896fdd004b5102bfb2f55c58c0ad106315dcc119
Ikanga93 <153716887+Ikanga93@users.noreply.github.com>
2024-09-05 22:45:31 -0400

    commit

---
+++ workplace/ofele/ofele_app/urls.py
@@ -0,0 +1,7 @@
+# IMport the necessary modules to call the view functions when the URL is requested.
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.moderator),
+]
--- workplace/ofele/ofele_app/views.py
+++ workplace/ofele/ofele_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+import random
+
+moderatorList = [
+    "Fr. Gilchrist",
+    "Maman Mado",
+    "Maman Caro",
+    "Maman Sandrine",
+    "Fr. Yves",
+    "Fr. Jeremie",
+    "Fr. Gentiny",
+    "Fr. Onesime",
+    "Maman Hadassa",
+    "Maman Olga",
+    "Maman Alphie",
+    "Maman Gisèle",
+    "Maman Mémé",
+    "Maman Bijou",
+]
+
+def moderator(request):
+    moderator = random.choice(moderatorList)
+    context = {"moderator": moderator}
+    return render(request, "ofele_app/ofele.html", context)
